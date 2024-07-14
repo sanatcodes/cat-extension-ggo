d93db7baeea4ade516b67f21b87970a7f7731914
Sanat <sanat.thukral@gmail.com>
2024-07-14 22:24:44 +0100

    fix: basic cat extension working now

--- background.js
+++
@@ -1,3 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Cat Image Replacer extension installed.");
-});
--- content.js
+++ content.js
@@ -1,41 +1,25 @@
-// function replaceImages() {
-//     const images = document.querySelectorAll('img');
-//     images.forEach((img) => {
-//       fetch('https://cataas.com/cat')
-//         .then(response => response.blob())
-//         .then(blob => {
-//           const url = URL.createObjectURL(blob);
-//           img.src = url;
-//         });
-//     });
-//   }
-
-//   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//     if (request.action === 'replaceImages') {
-//       replaceImages();
-//       sendResponse({ status: 'done' });
-//     }
-//   });
-
-const catImages = [
-  chrome.runtime.getURL("images/cat1.jpeg"),
-  chrome.runtime.getURL("images/cat2.jpeg"),
-  chrome.runtime.getURL("images/cat3.jpeg"),
+//array of images
+let catsImages = [
+  "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png",
+  "https://e3.365dm.com/21/03/768x432/skynews-cats-missing-microchip_5315182.jpg?20210323142004",
+  "https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fc3836660-7846-11eb-80c3-8cc375faed89.jpg?crop=5729%2C3222%2C187%2C805&resize=1200",
+  "https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/07/petting_pet_cat-1296x728-header.jpg?w=1155&h=1528",
 ];
 
-function replaceImages() {
-  const images = document.querySelectorAll("img");
-  images.forEach((img) => {
-    const randomCatImage =
-      catImages[Math.floor(Math.random() * catImages.length)];
-    img.src = randomCatImage;
-  });
+//reverse through array of images
+//getting random image from the array we created before (we use math.floor and math.random to grab a random index in the array)
+const imgs = document.getElementsByTagName("img");
+for (let i = 0; i < imgs.length; i++) {
+  const randomImg = Math.floor(Math.random() * catsImages.length);
+  imgs[i].src = catsImages[randomImg];
 }
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "replaceImages") {
-    console.log("Received replaceImages action");
-    replaceImages();
-    sendResponse({ status: "done" });
-  }
-});
+//do the same for h1 elements
+const headers = document.getElementsByTagName("h1");
+for (let i = 0; i < headers.length; i++) {
+  headers[i].innerText = "Cats are awesome.";
+}
+//do the same for p elements
+const p = document.getElementsByTagName("p");
+for (let i = 0; i < p.length; i++) {
+  p[i].innerText = "This website is now about cats.";
+}
